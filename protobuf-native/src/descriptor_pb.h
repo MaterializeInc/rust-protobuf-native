9da2b3066bc054d8a499a731dc2ca76b91ab510d
Nikhil Benesch <nikhil.benesch@gmail.com>
2021-12-25 13:27:35 -0500

    Add formatting script
    Which is capable of formatting C++. Also move the update script into the
    new bin directory.

--- protobuf-native/src/descriptor_pb.h
+++ protobuf-native/src/descriptor_pb.h
@@ -13,9 +13,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <memory>
-
 #include <google/protobuf/descriptor.pb.h>
+
+#include <memory>
 
 using namespace google::protobuf;
 
