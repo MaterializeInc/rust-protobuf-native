a015e080acb170fb4cd0e014aeacb6f2578f40df
Eric Marsden <eric.marsden@foncsi.org>
2024-07-21 15:03:13 +0200

    Replace subprocess calls to git by use of the git2-rs library

--- protobuf-src/build.rs
+++ protobuf-src/build.rs
@@ -14,36 +14,46 @@
 // limitations under the License.
 
 use std::env;
+use std::fs;
 use std::error::Error;
-use std::process::Command;
 use std::path::PathBuf;
+use git2::build::RepoBuilder;
+use git2::FetchOptions;
 
 // We check out the appropriate revision of the protobuf source code from its git repository,
 // instead of bundling the source code. It's also necessary to check out the abseil-cpp source code
 // separately, as this is not bundled with protobuf.
 fn main() -> Result<(), Box<dyn Error>> {
-    // Check out the protobuf source code
     let build_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    println!("Building in directory {:?}", build_dir);
-    assert!(Command::new("git")
-            .current_dir(build_dir.clone())
-            .arg("clone")
-            .arg("--depth=1")
-            .arg("--branch=v27.2")
-            .arg("https://github.com/protocolbuffers/protobuf")
-            .status()
-            .unwrap()
-            .success());
-    assert!(Command::new("git")
-            .current_dir(build_dir.join("protobuf").join("third_party"))
-            .arg("clone")
-            .arg("--depth=1")
-            .arg("https://github.com/abseil/abseil-cpp.git")
-            .status()
-            .unwrap()
-            .success());
+    let protobuf_dir = build_dir.join("protobuf");
+    let mut fo = FetchOptions::new();
+    fo.depth(1);
+    fo.download_tags(git2::AutotagOption::All);
+    if !protobuf_dir.exists() {
+        let r = RepoBuilder::new()
+            .fetch_options(fo)
+            .clone("https://github.com/protocolbuffers/protobuf", &protobuf_dir)
+            .expect("cloning protobuf repository");
+        let (object, reference) = r.revparse_ext("v27.2")
+            .expect("finding release tag");
+        r.checkout_tree(&object, None)
+            .expect("checking out release");
+        r.set_head(reference.unwrap().name().unwrap())
+            .expect("setting HEAD to release tag");
+    }
+    let abseil_dir = build_dir.join("protobuf").join("third_party").join("abseil-cpp");
+    let mut fo = FetchOptions::new();
+    fo.depth(1);
+    if abseil_dir.exists() &&
+        fs::read_dir(&abseil_dir).unwrap().count() == 0
+    {
+        RepoBuilder::new()
+            .fetch_options(fo)
+            .clone("https://github.com/abseil/abseil-cpp.git", &abseil_dir)
+            .expect("cloning abseil-cpp repository");
+    }
 
-    let install_dir = cmake::Config::new(build_dir.join("protobuf"))
+    let install_dir = cmake::Config::new(protobuf_dir)
         .define("ABSL_PROPAGATE_CXX_STD", "ON")
         .define("protobuf_BUILD_TESTS", "OFF")
         .define("protobuf_DEBUG_POSTFIX", "")
